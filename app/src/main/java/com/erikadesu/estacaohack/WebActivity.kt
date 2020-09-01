2cf62c0a011bf4218e154774405c897724e5530f
Erika <o.erika.o@gmail.com>
2020-09-01 10:18:04 -0400

    open cellep website on webview

--- app/src/main/java/com/erikadesu/estacaohack/WebActivity.kt
+++ app/src/main/java/com/erikadesu/estacaohack/WebActivity.kt
@@ -2,10 +2,21 @@ package com.erikadesu.estacaohack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_web.*
 
 class WebActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
+
+        //habilitando execucao de codigoo javascript
+        wbvSite.settings.javaScriptEnabled = true
+
+        //carregando site
+        wbvSite.loadUrl("http://br.cellep.com/estacaohack")
+
+        //definindo nosso webview como cliente web padrao
+        wbvSite.webViewClient = WebViewClient()
     }
 }
